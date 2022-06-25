64692d81f28790900e20d713d4b6cd7ae3a067b0
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-06-25 20:18:10 -0300

    Rendering Results

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Header from '../components/Header';
 import CurrentWall from '../components/CurrentWall';
 import WallBtns from '../components/WallBtns';
 import InputsList from '../components/InputsList';
+import Results from '../components/Results';
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
         <CurrentWall />
         <WallBtns />
         <InputsList />
+        <Results />
       </main>
     </>
   );
