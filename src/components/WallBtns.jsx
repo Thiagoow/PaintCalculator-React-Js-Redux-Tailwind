40115c81b7253cc89a56550eaa1ac123dbac4731
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-06-25 18:10:16 -0300

    Btns set as semibold + gap decreased

--- src/components/WallBtns.jsx
+++ src/components/WallBtns.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const WallBtns = () => {
   return (
-    <div className="flex justify-center mt-8 gap-12">
-      <button className="py-2 px-5 rounded-xl bg-bodyColor text-blueColor shadow-[1px_4px_8px_rgba(0,0,0,0.2)] hover:shadow-[1px_8px_16px_rgba(0,0,0,0.2)]">
+    <div className="flex justify-center mt-8 gap-10">
+      <button className="font-semibold text-sm py-2 px-5 rounded-xl bg-bodyColor text-blueColor shadow-[1px_4px_8px_rgba(0,0,0,0.2)] hover:shadow-[1px_8px_16px_rgba(0,0,0,0.2)]">
         Resetar
       </button>
 
-      <button className="py-2 px-5 rounded-xl bg-blueColor text-white shadow-[1px_4px_8px_rgba(29,161,242,0.5)] hover:shadow-[1px_8px_16px_rgba(29,161,242,0.5)]">
+      <button className="font-semibold text-sm py-2 px-5 rounded-xl bg-blueColor text-white shadow-[1px_4px_8px_rgba(29,161,242,0.5)] hover:shadow-[1px_8px_16px_rgba(29,161,242,0.5)]">
         Próxima
       </button>
     </div>
