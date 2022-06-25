808ad8a62eadfacd3c28896acc936c3bbdfe0df2
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-06-25 18:06:00 -0300

    Rendering main

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import Header from '../components/Header';
+import CurrentWall from '../components/CurrentWall';
+import WallBtns from '../components/WallBtns';
 
 export default function Home() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <main className="mt-[4.5rem] md:mt-20">
+        <CurrentWall />
+        <WallBtns />
+      </main>
+    </>
+  );
 }
