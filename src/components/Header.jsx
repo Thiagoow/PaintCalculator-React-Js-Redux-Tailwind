e9cd470e68ea7a10a38e0f51f2e9763f52292dc1
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-06-27 16:06:10 -0300

    Header zIndex repaired

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
   }
 
   return (
-    <header className="flex justify-between mx-5 mt-8 md:mx-20 lg:mx-auto lg:max-w-3xl self-center">
+    <header className="flex relative z-50 justify-between mx-5 mt-8 md:mx-20 lg:mx-auto lg:max-w-3xl self-center">
       <Logo />
 
       <nav className="h-3 justify-between align-middle">
@@ -24,7 +24,7 @@ function Header() {
           className={'md:flex' + (mobileMenu ? 'flex' : ' hidden')}
           id="nav-menu"
         >
-          <ul className="flex flex-col rounded-lg mt-5 py-5 bg-txtColor text-white text-center md:flex-row md:space-x-8 lg:space-x-8 md:py-0 md:text-black md:bg-transparent md:font-semibold">
+          <ul className="flex flex-col rounded-lg mt-10 py-5 bg-txtColor text-white text-center md:flex-row md:space-x-8 lg:space-x-8 md:py-0 md:text-black md:bg-transparent md:font-semibold">
             <li className="text-pinkColor cursor-pointer block py-4 md:py-0 hover:opacity-70">
               <a href="/">Home</a>
             </li>
