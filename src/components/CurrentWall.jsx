3496c3bbba43fd3e22fb78dd2ec805db79fc607e
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-06-28 20:23:14 -0300

    Showing currentWall from Redux state

--- src/components/CurrentWall.jsx
+++ src/components/CurrentWall.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import wallsIcon from '../../assets/img/Walls.svg';
+import { useSelector } from 'react-redux';
+import { selectCurrentWall } from '../slices/mainSlices';
 
 function CurrentWall() {
+  const currentWall = useSelector(selectCurrentWall);
+
   return (
     <div className="flex justify-center">
       <img src={wallsIcon} alt="Walls icons" />
 
       <p className="font-semibold text-txtColor ml-4 self-center">
-        Parede <b className="text-pinkColor">1</b> de 4
+        Parede <b className="text-pinkColor">{currentWall}</b> de 4
       </p>
     </div>
   );
