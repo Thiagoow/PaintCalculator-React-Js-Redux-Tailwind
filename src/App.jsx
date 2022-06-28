0c2eaa56222082c8d9b43219597125b77f029032
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-06-28 18:03:30 -0300

    Usign redux store with provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import Home from './pages/Home';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 export default function App() {
-  return <Home />;
+  return (
+    <Provider store={store}>
+      <Home />
+    </Provider>
+  );
 }
