4ee184993b5db34855fd01b08ccf7ff22643b43a
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-06-28 21:17:22 -0300

    Setting accumulator & disabling the insertBtn after 4 clicks

--- src/components/WallBtns.jsx
+++ src/components/WallBtns.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { setForm, goToNextWall, resetAll } from '../slices/mainSlices';
+import {
+  setForm,
+  goToNextWall,
+  resetAll,
+  setAccumulator
+} from '../slices/mainSlices';
 import calcAreas from '../utils/calcAreas';
 
 function WallBtns({ formData }) {
+  let [insertCLicked, setInsertCLicked] = React.useState(0);
   const dispatch = useDispatch();
 
   function ResetAllStates() {
+    setInsertCLicked(0);
     dispatch(resetAll());
   }
 
   function InsertOnRedux() {
+    setInsertCLicked(insertCLicked + 1);
+    //☝🏽 To disable button after 4 clicks
+
     const areas = calcAreas(
       formData.height,
       formData.width,
@@ -22,6 +32,7 @@ function WallBtns({ formData }) {
       alert(areas);
     } else {
       dispatch(setForm(formData));
+      dispatch(setAccumulator(areas));
       dispatch(goToNextWall());
     }
   }
@@ -36,10 +47,11 @@ function WallBtns({ formData }) {
       </button>
 
       <button
-        className="font-semibold text-sm py-2 px-5 rounded-xl bg-blueColor text-white shadow-[1px_4px_8px_rgba(29,161,242,0.5)] hover:shadow-[1px_8px_16px_rgba(29,161,242,0.5)]"
+        className="font-semibold text-sm py-2 px-5 rounded-xl bg-blueColor text-white shadow-[1px_4px_8px_rgba(29,161,242,0.5)] hover:shadow-[1px_8px_16px_rgba(29,161,242,0.5)] disabled:opacity-30 disabled:pointer-events-none"
         onClick={InsertOnRedux}
+        disabled={insertCLicked >= 4 ? true : false}
       >
-        Próxima
+        Calcular
       </button>
     </div>
   );
