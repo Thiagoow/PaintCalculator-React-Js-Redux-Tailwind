76add505aa6869cf435b940a433f6d7271c704f5
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-07-01 02:28:19 -0300

    Project finished!! 🤩🙌🏼🥳

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -7,28 +7,32 @@ import calcPaintNeed from '../utils/paintNeeded';
 function Results() {
   const { paintArea } = useSelector(selectAccumulator);
   const { liters, canQty } = calcPaintNeed(paintArea);
-  console.log(liters, canQty);
 
   return (
-    <section className="flex flex-col items-center my-16">
+    <section className="flex flex-col items-center my-12">
       <div className="flex flex-row">
         <img src={bucketIcon} alt="Bucket icon" />
 
-        <p className="font-bold text-txtColor ml-4 self-center">
+        <p className="font-semibold text-txtColor ml-4 self-center">
           Você vai precisar pintar:
         </p>
       </div>
 
       <div className="flex flex-col mt-5 items-center">
-        <p className="font-semibold text-txtColor">
+        <p className="font-medium text-txtColor mb-2">
           Uma área de <b className="font-bold text-pinkColor">{paintArea}m²</b>.
-          Ou seja:
         </p>
 
-        <p className="mt-4 font-semibold text-txtColor flex-wrap text-center">
-          Você precisa de {canQty[0]} lata{canQty[0] > 1 ? 's' : ''} de
-          <b className="text-blueColor"> {liters[0]} litros</b>
-        </p>
+        {canQty.map((num, i) => (
+          <p
+            className="mt-2 font-semibold text-txtColor flex-wrap text-center "
+            key={i}
+          >
+            {i + 1 > 1 ? '& ' : 'Para isso, use '}
+            {num} lata{num > 1 ? 's' : ''} de
+            <b className="text-blueColor"> {liters[i]} litros</b>
+          </p>
+        ))}
       </div>
     </section>
   );
