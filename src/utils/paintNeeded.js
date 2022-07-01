2f079e73ae4380c0313e564b1c92025a9363c0ad
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-06-30 23:28:23 -0300

    Calculating, just need to return it

--- src/utils/paintNeeded.js
+++ src/utils/paintNeeded.js
@@ -2,31 +2,33 @@
 const literCapacity = 5;
 
 const canOfPaints = {
-  tiny: 0.5,
-  small: 2.5,
+  large: 18,
   medium: 3.6,
-  large: 18
+  small: 2.5,
+  tiny: 0.5
 };
 
 //==========Main function:
 export default function calcPaintNeed(paintArea) {
-  const canModelsQty = Object.keys(canOfPaints).length;
-  //Calculate the number of paint needed:
-  const paintNeeded = paintArea / literCapacity;
+  //const canModelsQty = Object.keys(canOfPaints).length;
+  let paintNeeded = paintArea / literCapacity;
 
   if (paintArea < 1) {
     return 0;
   }
 
   Object.keys(canOfPaints).forEach((e) => {
-    //e: Attribute name | e.value: Attribute value;
-    const howMuch = paintNeeded / canOfPaints[e];
-    if (howMuch < 1) {
+    const iName = e;
+    const liters = canOfPaints[e];
+    const canQty = Math.round(paintNeeded / liters);
+
+    if (canQty < 1) {
       return 0;
     } else {
-      console.log(`${e}: ${howMuch}`);
+      console.log(`${liters}: ${canQty}`);
+      paintNeeded = paintNeeded - liters;
     }
   });
 
-  // return { liters, paints };
+  // return { liters, canQty };
 }
