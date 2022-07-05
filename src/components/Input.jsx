794efdfb44bb546945220c36a78aaf017d5e7cdd
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-07-05 14:37:12 -0300

    Unused React.fragment removed

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -7,30 +7,28 @@ function Input({ txtContent, iconName, rotateIcon, name, onlyInt }) {
   const { form, handleData } = formContext;
 
   return (
-    <>
-      <p
-        className={
-          'font-medium text-base items-center flex gap-6 md:gap-10 ' +
-          (rotateIcon ? '!gap-5 md:!gap-9' : '')
-        }
-      >
-        <Icon
-          icon={iconName}
-          className={'text-3xl ml-2 ' + (rotateIcon ? 'rotate-90' : '')}
-        />
-        {txtContent}
-        <input
-          type="number"
-          min="0"
-          max="50"
-          className="py-[0.2rem] rounded-md max-w-[77px]"
-          step={onlyInt ? 1 : 0.5}
-          name={name}
-          value={form[name]}
-          onChange={handleData}
-        />
-      </p>
-    </>
+    <p
+      className={
+        'font-medium text-base items-center flex gap-6 md:gap-10 ' +
+        (rotateIcon ? '!gap-5 md:!gap-9' : '')
+      }
+    >
+      <Icon
+        icon={iconName}
+        className={'text-3xl ml-2 ' + (rotateIcon ? 'rotate-90' : '')}
+      />
+      {txtContent}
+      <input
+        type="number"
+        min="0"
+        max="50"
+        className="py-[0.2rem] rounded-md max-w-[77px]"
+        step={onlyInt ? 1 : 0.5}
+        name={name}
+        value={form[name]}
+        onChange={handleData}
+      />
+    </p>
   );
 }
 
